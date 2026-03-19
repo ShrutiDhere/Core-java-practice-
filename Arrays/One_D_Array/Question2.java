75144fc08ada6e701b44027acc6fe018687946a1
Shruti Dhere <dhereshruti351@gmail.com>
2026-03-19 18:42:39 +0530

    Add files via upload

---
+++ Arrays/One_D_Array/Question2.java
@@ -0,0 +1,19 @@
+// Question 2 :- Write a program to print all elements of a 1D array.
+
+
+package One_D_Array;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		int num[]= {7,5,9,2,6,1};
+		for(int i=0;i<num.length;i++) {
+			
+		System.out.println(num[i]);
+		
+		}
+		
+		
+	}
+}
---
+++ Arrays/One_D_Array/Question3.java
@@ -0,0 +1,13 @@
+// Question 3 :- 
+
+package One_D_Array;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		int num[]= {1,2,3,4,5,6,7,8};
+		
+		System.out.println("The Length of the Array Elements is :- " + num.length);
+	}
+}
